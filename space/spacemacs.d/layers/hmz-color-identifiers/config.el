610d9ad711d403af6d52169d31bc8b0d92533e69
Fabio Mazarotto <fabio@mazarotto.me>
2018-02-20 15:21:04 -0300

    space was turned into spaceghost.d repository

--- space/spacemacs.d/layers/hmz-color-identifiers/config.el
+++
@@ -1,3 +0,0 @@
-(defvar hmz-color-identifiers-saturation 70
-  "Raises tab name so it vertically aligns with icon
-but it breaks underlines.")
--- space/spacemacs.d/layers/hmz-tabbar/config.el
+++
@@ -1,3 +0,0 @@
-(defvar hmz-tabbar-raise-text 0.2
-  "Raises tab name so it vertically aligns with icon
-but it breaks underlines.")
